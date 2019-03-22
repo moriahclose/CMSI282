f409a9b969f3ea577285a159f699abeb3148a7ee
mtollive <mtollive@lion.lmu.edu>
2019-03-21 21:37:32 -0700

    bottomUpTableFill complete

--- Homework/Homework03/lcs/LCS.java
+++ Homework/Homework03/lcs/LCS.java
@@ -1,5 +1,7 @@
 package lcs;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
@@ -32,12 +34,42 @@ public class LCS {
      *         [Side Effect] sets memoCheck to refer to table
      */
     public static Set<String> bottomUpLCS (String rStr, String cStr) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+    	memoCheck = bottomUpTableFill(rStr, cStr);
+    	return new HashSet<>(Arrays.asList(
+                "A"
+            ));
     }
     
     // [!] TODO: Add any bottom-up specific helpers here!
+    public static int[][] bottomUpTableFill( String s1, String s2 ) {
+    	int[][] memoTable = new int[s1.length()+1][s2.length()+1];
+    	String gutteredString1 = "0" + s1;
+    	String gutteredString2 = "0" + s2;
+    	
+    	for ( int row = 1; row < memoTable.length; row++ ) {
+    		for ( int col = 1; col < memoTable[0].length; col++ ) {
+    			if ( gutteredString1.charAt(row) == gutteredString2.charAt(col) ) {
+    				memoTable[row][col] = 1 + memoTable[row-1][col-1];
+    			} else {
+    				memoTable[row][col] = Math.max(memoTable[row-1][col], memoTable[row][col-1]);
+    			}
+    		}
+    	}
+    	
+    	print2DArray(memoTable);
+    	return memoTable;
+    }
     
     
+    public static void print2DArray( int[][] a ) {
+    	for ( int i = 0; i < a.length; i++) {
+    		for  (int j = 0; j < a[0].length; j++ ) {
+    			System.out.print(a[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    }
     // -----------------------------------------------
     // Top-Down LCS
     // -----------------------------------------------
@@ -57,5 +89,9 @@ public class LCS {
     
     // [!] TODO: Add any top-down specific helpers here!
     
+    public static void main(String args[]) {
+    	LCS l = new LCS();
+    	l.bottomUpTableFill("", "");
+    }
     
 }
