4499e3cd95a601ecb5a39ae7f2b576c2393008ac
mtollive <mtollive@lion.lmu.edu>
2019-03-25 16:34:28 -0700

    Added java docs

--- Homework/Homework03/lcs/LCS.java
+++ Homework/Homework03/lcs/LCS.java
@@ -1,3 +1,6 @@
+/******************************************
+ * Authors: Moriah Tolliver, Justin Wiggins
+ ******************************************/
 package lcs;
 
 import java.util.*;
@@ -15,25 +18,31 @@ public class LCS {
     // Shared Helper Methods
     // -----------------------------------------------
     
-    // [!] TODO: Add your shared helper methods here!
-    
-    public static void print2DArray( int[][] a ) {
-    	for ( int i = 0; i < a.length; i++) {
-    		for  (int j = 0; j < a[0].length; j++ ) {
-    			System.out.print(a[i][j] + " ");
-    		}
-    		System.out.println();
-    	}
-    }
-    
+    /**
+     * Returns the last character in a given string
+     * @param s String to get last character from
+     * @return char representing last character in string 
+     */
     public static char lastChar(String s) {
     	return s.charAt(s.length()-1);
     }
     
+    /**
+     * Returns given string without its last character
+     * @param s String to remove last character from
+     * @return String without last character of input string
+     */
     public static String removeLastChar(String s) {
     	return s.substring(0, s.length()-1);
     }
     
+    /**
+     * Returns a set of solutions to the given LCS problem
+     * @param rStr The String found along the table's rows
+     * @param cStr The String found along the table's cols
+     * @param memo 2D array representing the memoized table that has been constructed using bottom-up or top-down dynamic programming
+     * @return HashSet of String solutions to given LCS problem
+     */
     public static HashSet<String> collectSolution( String rStr, String cStr, int[][] memo ) {
     	if ( rStr.length() == 0 || cStr.length() == 0 ) {
     		return new HashSet<String>( Arrays.asList( "" ) );
@@ -67,21 +76,25 @@ public class LCS {
      * solves larger and larger subproblems iterative using a tabular
      * memoization structure.
      * @param rStr The String found along the table's rows
-     * @param cStr The String found along the table's cols
+     * @param cStr The String found along the table's columns
      * @return The longest common subsequence between rStr and cStr +
      *         [Side Effect] sets memoCheck to refer to table
      */
     public static Set<String> bottomUpLCS (String rStr, String cStr) {
-//        throw new UnsupportedOperationException();
     	memoCheck = bottomUpTableFill(rStr, cStr);
     	return collectSolution( rStr, cStr, memoCheck);
     }
     
-    // [!] TODO: Add any bottom-up specific helpers here!
-    public static int[][] bottomUpTableFill( String s1, String s2 ) {
-    	int[][] memoTable = new int[s1.length()+1][s2.length()+1];
-    	String gutteredString1 = "0" + s1;
-    	String gutteredString2 = "0" + s2;
+    /**
+     * Fills the memoization table using bottom-up dynamic programming
+     * @param rStr The String found along the table's rows
+     * @param cStr The String found along the table's cols
+     * @return 2D array containing the memoized values
+     */
+    public static int[][] bottomUpTableFill( String rStr, String cStr ) {
+    	int[][] memoTable = new int[rStr.length()+1][cStr.length()+1];
+    	String gutteredString1 = "0" + rStr;
+    	String gutteredString2 = "0" + cStr;
     	
     	for ( int row = 1; row < memoTable.length; row++ ) {
     		for ( int col = 1; col < memoTable[0].length; col++ ) {
@@ -109,13 +122,18 @@ public class LCS {
      *         [Side Effect] sets memoCheck to refer to table  
      */
     public static Set<String> topDownLCS (String rStr, String cStr) {
-//        throw new UnsupportedOperationException();
     	memoCheck = topDownTableFill(rStr, cStr, new int[rStr.length()+1][cStr.length()+1]);
     	return collectSolution( rStr, cStr, memoCheck);
     }
     
     // [!] TODO: Add any top-down specific helpers here!
     
+    /**
+     * Fills the memoization table using top-down dynamic programming
+     * @param rStr The String found along the table's rows
+     * @param cStr The String found along the table's cols
+     * @return 2D array containing the memoized values
+     */
     public static int[][] topDownTableFill( String s1, String s2, int[][] inputTable) {
     	int[][] memoTable = inputTable;
     	
@@ -130,12 +148,6 @@ public class LCS {
     	}
     	
     	return memoTable;
-    }
-    
-    public static void main(String args[]) {
-//    	LCS l = new LCS();
-////    	l.topDownTableFill("ABA", "BAA", new int[4][4]);
-//    	HashSet<String> b = new HashSet<>(Arrays.asList("hey", "there", "sir"));
     }
     
 }
