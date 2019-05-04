cb54ee15478b38451c497d66d92fbb509b2321c4
mtollive <mtollive@lion.lmu.edu>
2019-05-04 15:58:33 -0700

    Added node preprocessing

--- Homework/Homework05/CSP.java
+++ Homework/Homework05/CSP.java
@@ -1,12 +1,15 @@
 package csp;
 
+import static org.junit.Assert.fail;
+
 import java.time.LocalDate;
 import java.util.Set;
 
-import huffman.Huffman.HuffNode;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 /**
  * CSP: Calendar Satisfaction Problem Solver
@@ -15,6 +18,52 @@ import java.util.List;
  * on the dates of each meeting.
  */
 public class CSP {
+	
+	public static ArrayList<DateVar> nodePreprocess(ArrayList<DateVar> meetings, Set<DateConstraint> constraints) {
+		for (DateConstraint constraint : constraints) {
+    		DateVar newDateVar;
+    		
+            switch (constraint.OP) {
+            case "==": 
+            	if (meetings.get(constraint.L_VAL).dates.contains(((UnaryDateConstraint) constraint).R_VAL)) {
+	            	newDateVar = new DateVar(((UnaryDateConstraint) constraint).R_VAL);
+	            	meetings.set(constraint.L_VAL, newDateVar);
+            	} else {
+            		meetings = null;
+            	}
+            	break;
+            case "!=": 
+            	newDateVar = meetings.get(constraint.L_VAL);
+            	newDateVar.remove(((UnaryDateConstraint) constraint).R_VAL);
+            	meetings.set(constraint.L_VAL, newDateVar);
+            	break;
+            case ">":  
+            	newDateVar = meetings.get(constraint.L_VAL);
+            	newDateVar.remove(((UnaryDateConstraint) constraint).R_VAL);
+            	newDateVar.removeDatesLessThan(((UnaryDateConstraint) constraint).R_VAL);
+            	meetings.set(constraint.L_VAL, newDateVar);
+            	break;
+            case "<":  
+            	newDateVar = meetings.get(constraint.L_VAL);
+            	newDateVar.remove(((UnaryDateConstraint) constraint).R_VAL);
+            	newDateVar.removeDatesLessThan(((UnaryDateConstraint) constraint).R_VAL);
+            	meetings.set(constraint.L_VAL, newDateVar);
+            	break;
+            case ">=": 
+            	newDateVar = meetings.get(constraint.L_VAL);
+            	newDateVar.removeDatesLessThan(((UnaryDateConstraint) constraint).R_VAL);
+            	meetings.set(constraint.L_VAL, newDateVar);
+            	break;
+            case "<=": 
+            	newDateVar = meetings.get(constraint.L_VAL);
+            	newDateVar.removeDatesGreaterThan(((UnaryDateConstraint) constraint).R_VAL);
+            	meetings.set(constraint.L_VAL, newDateVar);
+            	break;
+            }
+            
+    	}
+		return meetings;
+	}
 
     /**
      * Public interface for the CSP solver in which the number of meetings,
@@ -28,9 +77,24 @@ public class CSP {
      *         indexed by the variable they satisfy, or null if no solution exists.
      */
     public static List<LocalDate> solve (int nMeetings, LocalDate rangeStart, LocalDate rangeEnd, Set<DateConstraint> constraints) {
-    	DateVar date = new DateVar(0, rangeStart, rangeEnd);
-       
-        return date.dates;
+    	// set all meeting vars with all dates in range and its corresponding index
+    	ArrayList<DateVar> meetings = new ArrayList<>();
+    	
+    	for ( int i = 0; i < nMeetings; i++) {
+    		DateVar date = new DateVar(rangeStart, rangeEnd);
+    		meetings.add(date);
+    	}
+
+    	// preprocessing
+    	meetings = nodePreprocess(meetings, constraints);
+    	
+    	// TO DELETE
+    	if (meetings != null ) {
+	        for (DateVar d : meetings) {
+	        	System.out.println( "     " + d );
+	        }
+    	}
+        return (meetings != null ) ? meetings.get(0).dates : null;
     }
     
  // -----------------------------------------------
@@ -42,27 +106,75 @@ public class CSP {
      */
     private static class DateVar {
         
-        int index;
         ArrayList<LocalDate> dates;
         
-        DateVar (int index, LocalDate start, LocalDate end) {
-            this.index = index;
+        DateVar (LocalDate start, LocalDate end) { // if given two dates will interpret as range
             this.dates = new ArrayList<>();
             
             while (start.until(end).getDays() >= 0) {
             	dates.add(start);
-            	System.out.print(start + " ") ;
             	start = start.plusDays(1);
             }
-            System.out.println();
+        }
+        
+        DateVar (LocalDate date) {
+        	this.dates = new ArrayList<>();
+        	dates.add(date);
         }
         
         public boolean remove(LocalDate date) {
         	return dates.remove(date);
         }
         
+        public boolean removeDatesGreaterThan(LocalDate date) {
+        	boolean didRemove = false;
+        	ArrayList<LocalDate> toRemove = new ArrayList<>(); 
+
+        	for (LocalDate d : dates) {
+        		if (d.compareTo(date) > 0) {
+        			toRemove.add(d);
+        			System.out.println( d + " > " + date);
+        			didRemove = true;
+        		}
+        	}
+        	
+        	for (LocalDate d : toRemove) {
+        		dates.remove(d);
+        	}
+        	
+        	return didRemove;
+        }
+        
+        public boolean removeDatesLessThan(LocalDate date) {
+        	boolean didRemove = false;
+        	ArrayList<LocalDate> toRemove = new ArrayList<>(); 
+
+        	for (LocalDate d : dates) {
+        		if (d.compareTo(date) < 0) {
+        			toRemove.add(d);
+        			System.out.println( d + " < " + date);
+        			didRemove = true;
+        		}
+        	}
+        	
+        	for (LocalDate d : toRemove) {
+        		dates.remove(d);
+        	}
+        	
+        	return didRemove;
+        }
+        
         public boolean add(LocalDate date) {
         	return dates.add(date);
+        }
+        
+        @Override
+        public String toString() {
+        	String dateStrings = "";
+        	for (LocalDate date : dates) {
+        		dateStrings += date + " ";
+        	}
+        	return dateStrings;
         }
         
     }
